b2a4d422c131f04d32734d5b3b7438a7125c1589
tkvang20x <letrungkien2007@gmail.com>
2022-10-30 12:15:44 +0700

    bvn js 1

---
+++ index.js
@@ -0,0 +1,18 @@
+let a= {}
+
+if (Object.keys(a).length ===0 ){
+    console.log("a is empty");
+}else{
+    console.log("a not is empty");
+}
+
+let b = 10
+if (b <= 5 ){
+    console.log("hoc luc trung binh");
+}else if (b >5 && b <6){
+    console.log("hoc luc kha");
+}else if (b >7 && b <=8){
+    console.log("hoc luc kha gioi");
+}else if (b >=9){
+    console.log("hoc luc gioi");
+}
